927aa60e79f9d1218ac88550c9b6302d3f4ceded
Isabella W <isabellawindhaber@gmail.com>
2022-11-15 11:45:33 +0100

    hotel booking system

---
+++ components/db_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$localhost = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "hotel_booking_system";
+
+// create connection
+$connect = new  mysqli($localhost, $username, $password, $dbname);
+
+// check connection
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+} 
+/*
+else {
+     echo "Successfully Connected";
+ }
+ */
